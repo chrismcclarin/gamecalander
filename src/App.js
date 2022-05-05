78fd0441ef19737f25c95fb775d071ddebe3c206
Chris McClarin <chrismcclarin@Chriss-Air.attlocal.net>
2022-05-05 03:16:43 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Calendar from 'react-calendar';
+import { useState } from 'react';
+import 'react-calendar/dist/Calendar.css';
+import './App.css';
+
+function App() {
+  const [date, setDate] = useState(new Date());
+  
+  
+  
+  
+  
+  return (
+    <div className="App">
+      <h1 className='text-center'>React Calendar</h1>
+      <div className='calendar-container'>
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <p className='text-center'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
+    </div>
+  );
+}
+
+export default App;
